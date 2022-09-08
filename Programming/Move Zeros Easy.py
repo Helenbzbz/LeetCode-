328605e10a7d7d3b991832dbe17a3823dc0dc512
Helen Zheng <jzheng3@babson.edu>
2022-09-08 12:31:55 -0400

    Create Move Zeros Easy.py

---
+++ Programming/Move Zeros Easy.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count = 0
+        i = 0
+        while count < len(nums):
+            if nums[i] == 0: 
+                nums.pop(i)
+                nums.append(0)
+            else: i+=1
+            count +=1
