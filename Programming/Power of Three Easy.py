2b91d4e818ccaf6bb82c18f48d84edac689cf812
Helen Zheng <jzheng3@babson.edu>
2022-09-12 09:23:10 -0400

    Create Power of Three Easy.py

---
+++ Programming/Power of Three Easy.py
@@ -0,0 +1,8 @@
+## Loop iteration is the more intuitive way
+import math
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n<1: return False
+        while n%3 == 0:
+            n = n/3
+        return n==1
