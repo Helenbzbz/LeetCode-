d8eaaab0e17a110cd7d11635a9503c9410b7ead0
Helen Zheng <jzheng3@babson.edu>
2022-09-16 11:05:50 -0400

    Create Logger Rate Limiter Easy.py

---
+++ Programming/Logger Rate Limiter Easy.py
@@ -0,0 +1,19 @@
+class Logger:
+
+    def __init__(self):
+        self.time = {}
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        if message in self.time.keys():
+            if timestamp >= self.time[message]:
+                self.time[message] = timestamp+10
+                return True
+            else: return False
+        else: 
+            self.time[message] = timestamp+10
+            return True
+        
+            
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
