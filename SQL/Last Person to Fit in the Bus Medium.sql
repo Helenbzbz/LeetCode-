fdf6ab9db7c8bb567ebd5e5bb21758014cfd93fe
Helen Zheng <jzheng3@babson.edu>
2022-09-24 14:08:11 -0400

    Update Last Person to Fit in the Bus Medium.sql

--- SQL/Last Person to Fit in the Bus Medium.sql
+++ SQL/Last Person to Fit in the Bus Medium.sql
@@ -4,3 +4,17 @@ GROUP BY q1.turn
 HAVING SUM(q2.weight) <= 1000
 ORDER BY SUM(q2.weight) DESC
 LIMIT 1
+
+with cumu_weight as (select
+    a.person_id,
+    sum(b.weight) as cumulative_weight,
+    a.turn,
+    a.person_name
+from Queue a join Queue b on a.turn >= b.turn
+group by 1,3,4)
+
+select
+    person_name
+from cumu_weight
+where cumulative_weight <= 1000
+order by cumulative_weight desc limit 1
